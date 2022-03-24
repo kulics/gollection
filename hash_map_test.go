b295ca64c563dbb0ebb156912deaf960bfb0b3ba
Kulics Wu <kulics@outlook.com>
2022-03-25 07:22:39 +0800

    add more map test

--- hash_map_test.go
+++ hash_map_test.go
@@ -1,21 +1,47 @@
 package gollection
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHashMap(t *testing.T) {
 	var hashmap = HashMapOf[string, int]()
 	if hashmap.Size() != 0 {
 		t.Fatal("map size not eq 0")
 	}
-	hashmap.Put("1", 1)
+	hashmap.Put("111", 1)
 	if hashmap.Size() != 1 {
 		t.Fatal("map size not eq 1")
 	}
-	hashmap.Put("1", 2)
+	hashmap.Put("111", 2)
 	if hashmap.Size() != 1 {
 		t.Fatal("map size not eq 1")
 	}
-	if v, ok := hashmap.TryGet("1").Get(); !ok || v != 2 {
+	if v, ok := hashmap.TryGet("111").Get(); !ok || v != 2 {
+		t.Fatal("map value not eq 2")
+	}
+	var strkey = fmt.Sprintf("%d", 111)
+	hashmap.Put(strkey, 3)
+	if hashmap.Size() != 1 {
+		t.Fatal("map size not eq 1")
+	}
+	if v, ok := hashmap.TryGet(strkey).Get(); !ok || v != 3 {
+		t.Fatal("map value not eq 3")
+	}
+	var hashmap2 = HashMapOf[int, int]()
+	if hashmap2.Size() != 0 {
+		t.Fatal("map size not eq 0")
+	}
+	hashmap2.Put(111, 1)
+	if hashmap2.Size() != 1 {
+		t.Fatal("map size not eq 1")
+	}
+	hashmap2.Put(111, 2)
+	if hashmap2.Size() != 1 {
+		t.Fatal("map size not eq 1")
+	}
+	if v, ok := hashmap2.TryGet(111).Get(); !ok || v != 2 {
 		t.Fatal("map value not eq 2")
 	}
 }
